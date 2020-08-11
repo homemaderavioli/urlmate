01a28d7add323972da880962ce94cf5811c3faf8
Ravel Ford <ravel.ford@protonmail.com>
2020-08-12 00:02:23 +1000

    tests n stuff

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module gitlab.com/urlmate/url
 
 go 1.14
+
+require (
+	github.com/basho/backoff v0.0.0-20150307023525-2ff7c4694083 // indirect
+	github.com/basho/riak-go-client v1.7.0
+	github.com/basho/taste-of-riak v0.0.0-20151229174444-e884ac570ec7
+	github.com/gin-gonic/gin v1.6.3
+)
--- url.go
+++ url.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"math/rand"
+	"time"
+)
 
-func main() {
-	fmt.Println("Hello, world.")
+// URL is the structure to hold information about a shorten url
+type URL struct {
+	OriginalURL    string
+	CreationDate   string
+	ExpirationDate string
+}
+
+// URLStorage interface
+type URLStorage interface {
+	CreateURL(key string, value []byte) (string, error)
+	GetURL(key string) ([]byte, error)
+}
+
+const (
+	twoYears = 8766 * 2
+)
+
+// SaveURL api
+func SaveURL(sc URLStorage, originalURL string) (string, error) {
+	creationDate := time.Now()
+	expirationDate := creationDate.Add(time.Hour * twoYears)
+	var url = &URL{
+		OriginalURL:    originalURL,
+		CreationDate:   creationDate.String(),
+		ExpirationDate: expirationDate.String(),
+	}
+	jsonURL, err := json.Marshal(url)
+	if err != nil {
+		return "", err
+	}
+
+	urlKey := generateURLKey(6)
+	key, err := sc.CreateURL(urlKey, jsonURL)
+	if err != nil {
+		return "", err
+	}
+	return key, nil
+}
+
+// FindURL api
+func FindURL(sc URLStorage, urlKey string) (string, error) {
+	rawURL, err := sc.GetURL(urlKey)
+	if err != nil {
+		return "", err
+	}
+	var url = &URL{}
+	err = json.Unmarshal(rawURL, url)
+	if err != nil {
+		return "", err
+	}
+	return url.OriginalURL, nil
+}
+
+func generateURLKey(length int) string {
+	buf := make([]byte, length)
+	_, err := rand.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	return base64.StdEncoding.EncodeToString(buf)
 }
